3ff519783ad94e50a1d7930ee97e8f9b66c1971c
Maxwell You <maxwell.you@mlb.com>
2022-06-03 15:15:47 -0600

    move provider var in appwrapper

--- app/AppWrapper.tsx
+++ app/AppWrapper.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import App from "./App";
 import TaskContext from "./models/Task";
-const { RealmProvider } = TaskContext;
 
 const AppWrapper = () => {
+  const {RealmProvider} = TaskContext;
+
   return (
     <RealmProvider>
       <App />
